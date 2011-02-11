09d35c72d6d658386321364a9bcb644c7a8f0eb2
Stefan Wienert <info@stefanwienert.net>
2011-02-11 21:32:32 +0100

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Germanize::Application.config.secret_token = '4701366231e8a610929051c81a90fa1ae9e8d9ac39077d5041e3323e5784627e491914b072f2a6e904a77d3d4c113bd80fd44085a256434c548309db808c57dc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Germanize::Application.config.session_store :cookie_store, :key => '_germanize_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Germanize::Application.config.session_store :active_record_store
